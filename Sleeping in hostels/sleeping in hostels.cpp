65822f4f934fbaf154969521f7039f8c7fe9bf82
ALUMNO-D103 <ALUMNO-D103@UCSP.AULAB>
2026-08-26 19:29:03 -0500

    sleeping in hostels

--- Sleeping in hostels/sleeping in hostels.cpp
+++ Sleeping in hostels/sleeping in hostels.cpp
@@ -4,16 +4,31 @@
 
 using namespace std;
 
+int dist(vector<string>caso)
+{
+	int dist_max = 0;
+	
+	return 0;
+}
+
 int main()
 {
 	int casos = 0;
 	cin>>casos;
 	string entrada = "";
+	vector<vector<string>>casos_totales;
 	for(int i = 0;i < casos;i++)
 	{
 		cin>>entrada;
 		vector<string>caso(entrada.length());
 		caso.push_back(entrada);
+		casos_totales.push_back(caso);
 		cout<<caso.size()<<endl;
 	}
+	vector<vector<string>>::iterator j = casos_totales.begin();
+	for(;j<casos_totales.end();j++)
+	{
+		dist(*j);
+	}
+	return 0;
 }
